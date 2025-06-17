dcd1dcd3a6e278616f629f44c619cc739bf13bd8
Mamad <mohammadpjsh@gmail.com>
2025-06-17 12:41:31 +0100

    new dependencies and bug fixes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getUsers } from "../controllers/userController";
+import { getUsers } from "../controllers/userController.js";
 
 const router = express.Router();
 router.get("/", getUsers);
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import userRoutes from "./routes/userRoutes.js";
-import { errorHandler } from "./middleware/errorHandler";
+import { errorHandler } from "./middleware/errorHandler.js";
 
 dotenv.config();
 const app = express();
