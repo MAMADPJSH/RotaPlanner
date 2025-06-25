cde6ff17465e8f7fd7e0bd5521d3c7ef1d901c4a
Mamad <mohammadpjsh@gmail.com>
2025-06-25 16:26:02 +0100

    uuuuh sorry not sorry

--- backend/controllers/userController.js
+++
@@ -1,6 +0,0 @@
-import * as userModel from "../models/userModel.js";
-
-export async function getUsers(req, res) {
-  const users = await userModel.getAllUsers();
-  res.json(users);
-}
--- backend/middleware/errorHandler.js
+++
@@ -1,6 +0,0 @@
-export function errorHandler(err, req, res, next) {
-  console.log(err.stack);
-  res.status(500).json({
-    error: "Something went wrong cuh!",
-  });
-}
